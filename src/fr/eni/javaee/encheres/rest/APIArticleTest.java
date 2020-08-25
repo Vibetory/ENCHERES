6495805e3acf79dc71aab84855f54dde3733cca1
dyrits <dyrits@outlook.fr>
2020-08-25 23:48:44 +0300

    API Test updated.

--- src/fr/eni/javaee/encheres/rest/APIArticleTest.java
+++ src/fr/eni/javaee/encheres/rest/APIArticleTest.java
@@ -2,23 +2,21 @@ package fr.eni.javaee.encheres.rest;
 
 import fr.eni.javaee.encheres.EException;
 import fr.eni.javaee.encheres.bo.Article;
-import fr.eni.javaee.encheres.bo.Utilisateur;
 import fr.eni.javaee.encheres.dal.jdbc.ArticleJDBCDAOImpl;
-import fr.eni.javaee.encheres.dal.jdbc.UtilisateurJDBCDAOImpl;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-@Path("/utilisateur")
-public class APIUtilisateurTest {
+@Path("/article")
+public class APIArticleTest {
 
 
     @GET
     @Path("/{identifier: \\d+}")
-    public Utilisateur selectById(@PathParam("identifier") int identifier)  {
+    public Article selectById(@PathParam("identifier") int identifier)  {
         try {
-            return new UtilisateurJDBCDAOImpl().selectById(identifier);
+            return new ArticleJDBCDAOImpl().selectById(identifier);
         } catch (EException eException) {
             eException.printStackTrace();
         }
